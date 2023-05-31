f66891f9a1071ee80c348f0867fa3b7f3ca95f33
Mosesbwire <mosesdev92@gmail.com>
2023-05-31 10:52:58 +0000

    Add function to check if node is a leaf node

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: Pointer to the node to check
+ * Return: 1 if node is leaf otherwise 0
+ */
+
+int _binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left || node->right || !node)
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
+/**
  * binary_tree_leaves - counts the leaves in a binary tree
  *
  * @tree: Pointer to the root node of the tree to count
@@ -14,7 +31,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_is_leaf(tree) == 1)
+	if (_binary_tree_is_leaf(tree) == 1)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
