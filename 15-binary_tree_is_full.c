267340723fa6ca57b11cde913e4a4ecc7874efc7
Mosesbwire <mosesdev92@gmail.com>
2023-05-31 11:12:25 +0000

    add function to check tree height

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: 0 if tree is NULL otherwise Return size
+ */
+
+size_t _binary_tree_size(const binary_tree_t *tree)
+{
+
+	if (!tree)
+		return (0);
+	return (1 + _binary_tree_size(tree->left) + _binary_tree_size(tree->right));
+
+}
+/**
  * binary_tree_is_full - checks if a binary tree is full
  *
  * @tree: Pointer to root node of the tree
@@ -15,7 +31,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	total_nodes = binary_tree_size(tree);
+	total_nodes = _binary_tree_size(tree);
 
 	if (total_nodes % 2 == 0)
 		return (0);
