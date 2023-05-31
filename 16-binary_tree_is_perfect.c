c191e39124a18aebd2dbe66315b8946832a74317
Mosesbwire <mosesdev92@gmail.com>
2023-05-31 11:17:37 +0000

    Add function to check balance factor of tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,47 @@
 #include "binary_trees.h"
-#include <stdio.h>
+
+/**
+ * _binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: 0 if tree is NULL otherwise return height
+ */
+
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left;
+	size_t height_right;
+
+	if (!tree)
+		return (0);
+
+	height_left = tree->left ? 1 + _binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + _binary_tree_height(tree->right) : 0;
+
+	return (height_left > height_right ? height_left : height_right);
+
+}
+/**
+ * _binary_tree_balance - measures the balance factor of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: 0 if tree is NULL otherwise return balance factor
+ */
+
+int _binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_left;
+	int height_right;
+
+	if (!tree)
+		return (0);
+
+	height_left = tree->left ? 1 + _binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + _binary_tree_height(tree->right) : 0;
+	return (height_left - height_right);
+}
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  *
@@ -15,7 +57,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	balance_factor = binary_tree_balance(tree);
+	balance_factor = _binary_tree_balance(tree);
 
 	if (balance_factor != 0)
 		return (0);
